81c1df0544003e09d3e399b0b801b315cda80815
Vladislav Shatravin <vladiksat@list.ru>
2019-04-08 19:41:14 +0700

    Added unit tests

---
+++ NNDataGen.Tests/UnitTests/RandomGeneratedSampleUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using NNDataGen.Classes;
+using NNDataGen.Samples;
+using NSubstitute;
+using NSubstitute.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace NNDataGen.Tests.UnitTests
+{
+    public class RandomGeneratedSampleUnitTests
+    {
+
+        private Random _randomSubstitute;
+        private readonly Class1 _class1;
+        private readonly Class2 _class2;
+        private List<BaseClass> _classes;
+
+        public RandomGeneratedSampleUnitTests()
+        {
+            _randomSubstitute = Substitute.For<Random>();
+            _class1 = new Class1();
+            _class2 = new Class2();
+
+            _classes = new List<BaseClass> { _class1, _class2 };
+        }
+
+        [InlineData(0.0001, 0.001, 0)]
+        [InlineData(0.45, 0.001, 0)]
+        [InlineData(0.0001, 0.45, 0)]
+        [InlineData(0.45, 0.45, 0)]
+        [InlineData(0.55, 0.55, 0)]
+        [InlineData(0.99, 0.55, 0)]
+        [InlineData(0.55, 0.99, 0)]
+        [InlineData(0.99, 0.99, 0)]
+
+        [InlineData(0.0001, 0.99, 1)]
+        [InlineData(0.0001, 0.55, 1)]
+        [InlineData(0.45, 0.99, 1)]
+        [InlineData(0.45, 0.55, 1)]
+        [InlineData(0.99, 0.001, 1)]
+        [InlineData(0.99, 0.45, 1)]
+        [InlineData(0.55, 0.45, 1)]
+        [InlineData(0.55, 0.001, 1)]
+        [Theory]
+        public void Sample_CorrectSelectClass(double feature1, double feature2, int classKey)
+        {
+            _randomSubstitute.Configure().NextDouble().Returns(feature1, feature2);
+            var sample = new RandomGeneratedSample(_randomSubstitute);
+
+            //check classes was not changed
+            var satisfiedClass = _classes.First(c => c.ClassKey == classKey);
+            satisfiedClass.IsValueSatisfied(sample).Should().BeTrue();
+
+            sample.SetSatisfiedClass(_classes);
+            sample.ClassKey.Should().Be(classKey);
+        }
+    }
+}
---
+++ NNDataGen.Tests/UnitTests/RandomSamplesGeneratorUnitTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using NNDataGen.Classes;
+using NSubstitute;
+using NSubstitute.Extensions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace NNDataGen.Tests.UnitTests
+{
+    public class RandomSamplesGeneratorUnitTests
+    {
+        private Random _random;
+
+        public RandomSamplesGeneratorUnitTests()
+        {
+            _random = Substitute.For<Random>();
+        }
+
+        [Fact]
+        public void Generator_IgnoresDuplicates()
+        {
+            int samplesCount = 2;
+
+            double feature1_1 = 0.25;
+            double feature2_1 = 0.35;
+            double feature1_2 = 0.52;
+            double feature2_2 = 0.3;
+
+            _random.Configure()
+                .NextDouble()
+                .Returns(
+                    feature1_1, feature2_1,
+                    feature1_1, feature2_1,
+                    feature1_2, feature2_2);
+
+            var generator = new RandomSamplesGenerator(samplesCount,
+                new List<BaseClass> { new Class1(), new Class2() },
+                _random);
+
+            var samples = generator.GenerateSamples();
+
+            samples.Should().HaveCount(samplesCount);
+            samples[0].Feature1.Should().Be(feature1_1);
+            samples[0].Feature2.Should().Be(feature2_1);
+            samples[0].ClassKey.Should().Be(0);
+
+            samples[1].Feature1.Should().Be(feature1_2);
+            samples[1].Feature2.Should().Be(feature2_2);
+            samples[1].ClassKey.Should().Be(1);
+        }
+    }
+}
