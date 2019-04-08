cc2ec7c72c9c35f0feeeb8a0b451e08fe5080275
Vladislav Shatravin <vladiksat@list.ru>
2019-04-08 19:40:39 +0700

    Added samples generator

---
+++ NNDataGen/RandomSamplesGenerator.cs
@@ -0,0 +1,40 @@
+﻿using NNDataGen.Classes;
+using NNDataGen.Samples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NNDataGen
+{
+    public class RandomSamplesGenerator
+    {
+        private readonly int _samplesCount;
+        private readonly IEnumerable<BaseClass> _classes;
+        private readonly Random _random;
+
+        public RandomSamplesGenerator(int samplesCount,
+            IEnumerable<BaseClass> classes, Random random)
+        {
+            _samplesCount = samplesCount;
+            _classes = classes;
+            _random = random;
+        }
+
+        public List<RandomGeneratedSample> GenerateSamples()
+        {
+            var uniqueSamplesDict = new Dictionary<(double, double), RandomGeneratedSample>();
+            for (int i = 0; i < _samplesCount;)
+            {
+                var sample = new RandomGeneratedSample(_random);
+                sample.SetSatisfiedClass(_classes);
+                if (uniqueSamplesDict.ContainsKey((sample.Feature1, sample.Feature2)))
+                    continue;
+
+                uniqueSamplesDict.Add((sample.Feature1, sample.Feature2), sample);
+                i++;
+            }
+
+            return uniqueSamplesDict.Values.ToList();
+        }
+    }
+}
